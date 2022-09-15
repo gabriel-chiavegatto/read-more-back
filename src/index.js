11775e565181063a3e6d498a4de035a7955b84af
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-09-14 21:40:42 -0300

    keroku deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+dotenv.config();
 
-server.get("/",(req,res)=>{
-    
+server.get("/", async(req, res) => {
+    console.log("try")
 });
 
 
-server.listen(process.env.PORT, ()=>{console.log("Hello World")})
+server.listen(process.env.PORT, () => { console.log("Hello World") })
