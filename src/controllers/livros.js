fd52afe28cb2290b03f80d20553e72839060bcba
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-09-15 21:40:07 -0300

    adicionar e listar livros

--- src/controllers/livros.js
+++ src/controllers/livros.js
@@ -1,13 +1,65 @@
-import {db ,ObjectId} from "../database.js";
-
-async function listarTodosOsLivros(req,res){
-    res.sendStatus(200);
-}
-async function listarCarrinho(req,res){
-    try{
-        res.sendStatus(200);
-    } catch(error){
+import { db, ObjectId } from "../database.js";
+import dotenv from "dotenv";
+dotenv.config();
+const collectionBooks = "books-test";
+async function listarTodosOsLivros(req, res) {
+    try {
+        const books = await db.collection(collectionBooks).find().toArray();
+        res.send(books).status(200);
+    } catch (error) {
         res.sendStatus(500);
     }
 }
-export {listarTodosOsLivros, listarCarrinho}
+async function adicionarNovoLivro(req, res) {
+    try {
+        const { titulo, autor, preco, descricao, estoque } = req.body;
+        const insertBook = {
+            titulo,
+            autor,
+            preco,
+            descricao,
+            estoque
+        }
+        await db.collection(collectionBooks).insertOne(insertBook)
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+async function listarPropriedadesDoLivro(req, res) {
+    try {
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+async function listarCarrinho(req, res) {
+    try {
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+async function comprarCarrinho(req, res) {
+    try {
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+async function comprarLivro(req, res) {
+    try {
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+export {
+    listarTodosOsLivros,
+    listarCarrinho,
+    listarPropriedadesDoLivro,
+    adicionarNovoLivro,
+    comprarCarrinho,
+    comprarLivro
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {listarTodosOsLivros} from "./controllers/livros.js"
+import {
+    listarCarrinho,
+    listarTodosOsLivros,
+    adicionarNovoLivro,
+    listarPropriedadesDoLivro,
+    comprarCarrinho,
+    comprarLivro
+} from "./controllers/livros.js"
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 dotenv.config();
 
-server.get("/", async(req, res) => {
+server.get("/", async (req, res) => {
     console.log("try");
     res.status(200).send("Hello my World")
 });
-server.get("/store", listarTodosOsLivros)
+server.get("/biblioteca", listarTodosOsLivros);
+server.post("/biblioteca", adicionarNovoLivro);
+server.get("/livro/:id", listarPropriedadesDoLivro);
+server.get("/carrinho", listarCarrinho);
+server.post("/finalizar-compra-carrinho", comprarCarrinho);
+server.post("/finalizar-compra-livro", comprarLivro);
 
 
 server.listen(process.env.PORT, () => { console.log("Hello World") })
